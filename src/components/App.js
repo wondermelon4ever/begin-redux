9b711206f93b1c1497dbb44da218e0adf3ef9678
wondermelon4ever <wondermelonforever@gmail.com>
2020-09-27 13:00:04 +0900

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import AppTemplate from './AppTemplate';
-import Counter from './Counter';
-import Todos from './Todos';
+import CounterContainer from 'containers/CounterContainer';
+// import Counter from './Counter';
+// import Todos from './Todos';
+import TodosContainer from 'containers/TodosContainer';
 
 class App extends Component {
   render() {
     return (
       <AppTemplate
-        counter={<Counter />}
-        todos={<Todos />}
+        counter={<CounterContainer />}
+        todos={<TodosContainer />}
       />
     );
   }
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,1 +1,10 @@
 // 모든 모듈들을 불러와서 합치는 작업이 이뤄짐
+
+import { combineReducers } from 'redux';
+import counter from './counter';
+import todo from './todo';
+
+// 다른 reducer가 있으면 아래 코드에 추가해 주면 됨
+export default combineReducers({
+    counter, todo
+});
